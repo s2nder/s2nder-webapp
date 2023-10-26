ea970212869924b3005bbe86cefd71a0cb14fc54
s2nder <83005169+s2nder@users.noreply.github.com>
2023-10-26 11:47:08 -0400

    Change AMi

--- datasources.tf
+++ datasources.tf
@@ -3,5 +3,6 @@
 ##################################################################################
 
 data "aws_ssm_parameter" "amzn2_linux" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+  #name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-kernel-5.10-hvm-x86_64-gp2"
 }
