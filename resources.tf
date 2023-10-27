09e95d840036f0c878939e99baf21df19d48a169
s2nder <83005169+s2nder@users.noreply.github.com>
2023-10-27 18:19:01 -0400

    Switch to using secrets manager

--- resources.tf
+++ resources.tf
@@ -25,11 +25,19 @@ locals {
 # RESOURCES
 ##################################################################################
 
+resource "aws_iam_instance_profile" "main" {
+  name = "${local.name_prefix}-webapp"
+  role = var.ec2_role_name
+
+  tags = local.common_tags
+}
+
 resource "aws_instance" "main" {
   #count        = length(var.public_subnets)
-  count         = length(data.tfe_outputs.networkin.nonsensitive_values.public_subnets)
-  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type = var.instance_type
+  count                = length(data.tfe_outputs.networkin.nonsensitive_values.public_subnets)
+  ami                  = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  iam_instance_profile = aws_iam_instance_profile.main.name
+  instance_type        = var.instance_type
   #subnet_id    = var.public_subnets[count.index]
   subnet_id = data.tfe_outputs.networkin.nonsensitive_values.public_subnets[count.index]
   vpc_security_group_ids = [
@@ -72,6 +80,9 @@ resource "aws_instance" "main" {
 
   user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
+    secret_id           = var.api_key_secret_id
+    host_list_ssm_name  = local.host_list_ssm_name
+    site_name_ssm_name  = local.site_name_ssm_name
   })
 }
 
@@ -103,6 +114,8 @@ resource "null_resource" "webapp" {
 }
 */
 
+# deprecated after m9
+/*
 resource "terraform_data" "webapp" {
   triggers_replace = [
     length(aws_instance.main.*.id),
@@ -126,6 +139,7 @@ resource "terraform_data" "webapp" {
     private_key = module.ssh_keys.private_key_openssh
   }
 }
+*/
 
 resource "aws_lb" "main" {
   name               = "${local.name_prefix}-webapp"
